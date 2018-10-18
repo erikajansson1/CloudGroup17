87ef8406454cdf17db00848d04a1cf9f6955056b
Hung Huy Phan <phanhuyhung1707@gmail.com>
2018-10-18 14:57:47 +0200

    Delete config.py

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-# instance/config.py
-
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ujk3rlb7z2oar7ff:KxSsPqgwuoZKM2n5HGC@bmzyuuofr-mysql.services.clever-cloud.com/bmzyuuofr'
