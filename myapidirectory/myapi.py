114bb15396dfecde00cf7471ad10f9f823e2d3cb
Joakim Ingnäs <joakim.ingnas@gmail.com>
2018-10-25 09:32:17 +0200

    Fixed Allowed extensinos

--- myapidirectory/myapi.py
+++ myapidirectory/myapi.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'csv'])
 
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
